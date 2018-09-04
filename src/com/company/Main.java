7af7b5195fb11952f2250e4ea9671a4b3c6b669b
drobinatatjana <drobina.tatjana@gmail.com>
2018-09-04 19:58:48 +0300

    04 sept q

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,32 +13,32 @@ public class Main {
         do {
 
 
-        int myNum = rand.nextInt(100) + 1;
-        System.out.println(myNum);
+            int myNum = rand.nextInt(100) + 1;
+            System.out.println(myNum);
 
-        boolean userLost = true;
+            boolean userLost = true;
 
-        for (int i = 0; i < 3; i++) {
-            int userNum = scan.nextInt();
-            if (myNum > userNum) {
-                System.out.println("your number is less");
-            } else if (myNum < userNum) {
-                System.out.println("your number is bigger");
-            } else {
-                System.out.println("bingo");
-                userLost = false;
-                break;
+            for (int i = 0; i < 3; i++) {
+                int userNum = scan.nextInt();
+                if (myNum > userNum) {
+                    System.out.println("your number is less");
+                } else if (myNum < userNum) {
+                    System.out.println("your number is bigger");
+                } else {
+                    System.out.println("bingo");
+                    userLost = false;
+                    break;
+                }
             }
+            if (userLost == true) {
+                System.out.println("you are lost");
+                /*comment*/
+            }
+            System.out.println("one more game? print yes or no");
+            answer = askYN();
         }
-        if (userLost == true) {
-            System.out.println("you are lost");
-            /*comment*/
-        }
-        System.out.println("one more game? print yes or no");
-        answer = askYN();
-    }
-    while (answer.equals("yes"));
-    System.out.println("good buy!");
+        while (answer.equals("yes"));
+        System.out.println("good buy!");
     }
 
     static String askYN() {
@@ -47,11 +47,11 @@ public class Main {
             answer = scan.next();
             if (!answer.equals("yes") && !answer.equals("no")) {
                 System.out.println("you van enter only yes or no!");
-                continue;
+                continue; // here it is not obligatory
             } else {
-            break;
-        }
-        } while (true);
+                break;
+            }
+        } while (true); // endless cycle
 
         return answer;
     }
