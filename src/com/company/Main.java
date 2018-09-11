6d4a696cfac87f352dfa2a859b49a8781f410046
drobinatatjana <drobina.tatjana@gmail.com>
2018-09-11 20:29:58 +0300

    04 sept qq

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,20 @@
 package com.company;
 
-import java.util.InputMismatchException;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     static Random rand = new Random();
     static Scanner scan = new Scanner(System.in);
+    static List<Gameresult> results = new ArrayList<>();
 
     public static void main(String[] args) {
         String answer;
         do {
-
+            String name;
+            System.out.println("Input your name");
+            name = scan.next();
+            Long t1=System.currentTimeMillis();
 
             int myNum = rand.nextInt(100) + 1;
             System.out.println(myNum);
@@ -28,19 +30,42 @@ public class Main {
                     System.out.println("your number is bigger");
                 } else {
                     System.out.println("bingo");
+                    Long t2=System.currentTimeMillis();
+                    Long duration = (t2-t1)/1000;
+                    System.out.println("Your game duration was " + duration);
                     userLost = false;
+                    Gameresult r = new Gameresult();
+                    r.name=name;
+                    r.triesCount = i+1;
+                    results.add(r);
+
                     break;
+
                 }
             }
             if (userLost == true) {
                 System.out.println("you are lost");
                 /*comment*/
             }
+
             System.out.println("one more game? print yes or no");
             answer = askYN();
         }
         while (answer.equals("yes"));
+
+        showResults();
+
+
+
+
+
         System.out.println("good buy!");
+    }
+
+    private static void showResults() {
+        for(Gameresult r: results){
+            System.out.println(r.name + "-->" + r.triesCount);
+        }
     }
 
     static String askYN() {
